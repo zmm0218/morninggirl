f0e75ae4f86f57da856e359aeea8fed510fa50b4
zmm0218 <102661143+zmm0218@users.noreply.github.com>
2022-08-11 11:18:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_count():
 def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
-    next = next.replace(year=next.year + 1)
+    next = next.replace(year=next.year)
   return (next - today).days
 
 def get_words():
